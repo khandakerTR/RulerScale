69516ff8816e5dad1d5507bd53c07f352c7bbd81
na-khandaker <56843185+tkhandaker101@users.noreply.github.com>
2022-10-23 11:47:46 +0600

    Initial Commit

---
+++ RulerScale/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RulerScale
+//
+//  Created by BCL-Device-11 on 23/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
